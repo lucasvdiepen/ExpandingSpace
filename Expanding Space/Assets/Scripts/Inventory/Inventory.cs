02d4cc21f551005ed7ccb4f781c2c907262c8c3c
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-05-25 11:57:21 +0200

    Added basic inventory functions

--- Expanding Space/Assets/Scripts/Inventory/Inventory.cs
+++ Expanding Space/Assets/Scripts/Inventory/Inventory.cs
@@ -6,14 +6,60 @@ public class Inventory : MonoBehaviour
 {
     private List<GameObject> items = new List<GameObject>();
 
+    public GameObject testItem;
+
     public enum AllItems
     {
         Diamond = 0,
         Emerald = 1
     }
 
+    private AllItems GetItemType(GameObject item)
+    {
+        Item itemScript = item.GetComponent<Item>();
+        return itemScript.item;
+    }
+
+    private AllItems[] GetAllItemTypes()
+    {
+        AllItems[] allItems = new AllItems[items.Count];
+
+        for(int i = 0; i < items.Count; i++)
+        {
+            allItems[i] = GetItemType(items[i]);
+        }
+
+        return allItems;
+    }
+
     public void AddToInventory(GameObject item)
     {
         items.Add(item);
+    }
+
+    public void RemoveItem(AllItems item)
+    {
+
+    }
+
+    public bool HasItem(AllItems item)
+    {
+        foreach(AllItems currentItem in GetAllItemTypes())
+        {
+            if (currentItem == item) return true;
+        }
+
+        return false;
+    }
+
+    public int GetItemAmount(AllItems item)
+    {
+        int count = 0;
+        foreach(AllItems currentItem in GetAllItemTypes())
+        {
+            if (currentItem == item) count++;
+        }
+
+        return count;
     }
 }
