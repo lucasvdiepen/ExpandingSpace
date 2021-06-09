37f11ede551c11198fcece69c66f624d179a7abd
Scott van den Broek <you@example.com>
2021-06-09 13:46:14 +0200

    Added The BackGround animation and added the buttons

--- Expanding Space/Assets/Scripts/Main Menu/MainMenu.cs
+++ Expanding Space/Assets/Scripts/Main Menu/MainMenu.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Play()
+    {
+        Debug.Log("Play the Game");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Go to Options");
+    } 
     public void Quit()
     {
+        Debug.Log("Quit Game");
         // Quits game
         Application.Quit();
     }
+
 }
