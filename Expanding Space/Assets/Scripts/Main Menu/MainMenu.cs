2fab226623f0be782c5f136447fb56b66e4a924b
Scott van den Broek <you@example.com>
2021-06-12 18:29:44 +0200

    Added Settings button animation and added simple setting buttons

--- Expanding Space/Assets/Scripts/Main Menu/MainMenu.cs
+++ Expanding Space/Assets/Scripts/Main Menu/MainMenu.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject Butttons;
+
+    public Animator mainCamera;
     public void Play()
     {
         Debug.Log("Play the Game");
@@ -13,7 +16,11 @@ public class MainMenu : MonoBehaviour
     public void Options()
     {
         Debug.Log("Go to Options");
+        Butttons.SetActive(false);
+
+        mainCamera.SetTrigger("ZoomInToPlanet");
     } 
+
     public void Quit()
     {
         Debug.Log("Quit Game");
