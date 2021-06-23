175408c71fa11aaadd0286da8102eb471c97d199
lucasvdiepen <lucascool@ziggo.nl>
2021-06-23 11:30:49 +0200

    Planet click starts the travel game

--- Expanding Space/Assets/Scripts/Map/MapManager.cs
+++ Expanding Space/Assets/Scripts/Map/MapManager.cs
@@ -9,11 +9,26 @@ public class MapManager : MonoBehaviour
 
     public AllItems[] itemsLevel2, itemsLevel3, itemsLevel4, itemsLevel5, itemsLevel6, itemsLevel7, itemsLevel8, itemsLevel9;
 
+    private List<AllItems[]> requiredLevelItems;
+
+    private void Start()
+    {
+        requiredLevelItems = new List<AllItems[]>();
+        requiredLevelItems.Add(itemsLevel2);
+        requiredLevelItems.Add(itemsLevel3);
+        requiredLevelItems.Add(itemsLevel4);
+        requiredLevelItems.Add(itemsLevel5);
+        requiredLevelItems.Add(itemsLevel6);
+        requiredLevelItems.Add(itemsLevel7);
+        requiredLevelItems.Add(itemsLevel8);
+        requiredLevelItems.Add(itemsLevel9);
+    }
+
     private void OnEnable()
     {
         for(int i = 0; i < planetButtons.Length; i++)
         {
-            AddOnclickListener(i);
+            AddOnClickListener(i);
         }
     }
 
@@ -25,13 +40,31 @@ public class MapManager : MonoBehaviour
         }
     }
 
-    public void AddOnclickListener(int i)
+    public void AddOnClickListener(int i)
     {
-        planetButtons[i].onClick.AddListener(() => PlanetClicked(i + 1));
+        planetButtons[i].onClick.AddListener(() => PlanetClicked(i));
+    }
+
+    private bool IsPlanetUnLocked(int id)
+    {
+        foreach(AllItems item in requiredLevelItems[id])
+        {
+            if (!FindObjectOfType<Inventory>().HasItem(item)) return false;
+        }
+
+        return true;
     }
 
     private void PlanetClicked(int planetId)
     {
         Debug.Log("Planet clicked " + planetId);
+
+        if(planetId > 0)
+        {
+            //Check if player has all items
+            if (!IsPlanetUnLocked(planetId - 1)) return;
+        }
+
+        PlanetSelection.LoadTravelGame((PlanetSelection.Planets)planetId);
     }
 }
