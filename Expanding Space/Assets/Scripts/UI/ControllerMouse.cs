e0f06af57306982d8d9c3a8a927f39727f56e3b2
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-06-24 00:29:01 +0200

    Started working on controller ui support

---
+++ Expanding Space/Assets/Scripts/UI/ControllerMouse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerMouse : MonoBehaviour
+{
+    //For clicking buttons with controller
+    //https://stackoverflow.com/questions/46198428/generate-clickmousebutton0-anywhere-on-display
+
+    public void SetControllerMouse(bool enable)
+    {
+
+    }
+
+    void Update()
+    {
+        //Mouse.current.WarpCursorPosition(new Vector2(100, 0));
+    }
+}
